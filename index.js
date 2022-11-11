a87156df93187637c5f1838cd0b64f2e26f4f864
AnneKin <annekhd98@gmail.com>
2022-11-11 18:42:55 -0300

    more config

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ import cors from 'cors'
 const app = express()
 dotenv.config()
 app.use(express.json())
+app.use(cors())
 
 
 const mongoClient = new MongoClient(process.env.MONGO_URI)
 
 try{
     await mongoClient.connect()
+    console.log("MongoClient connected!")
 }
 catch(err){
 console.log(err)
